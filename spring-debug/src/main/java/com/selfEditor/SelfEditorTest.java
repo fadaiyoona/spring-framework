06efa6ab3fa4e558a280500d9c88bef8b73602cb
alin <576339702@qq.com>
2021-11-19 17:31:37 +0800

    1、添加自定义标签的测试类 2、添加测试包的readme文档

---
+++ spring-debug/src/main/java/com/selfEditor/SelfEditorTest.java
@@ -0,0 +1,13 @@
+package com.selfEditor;
+
+import com.other.Student;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SelfEditorTest {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("selfEditor.xml");
+        Customer bean = ac.getBean(Customer.class);
+        System.out.println(bean);
+	}
+}
